3818a1d790d59caa5b4b4bf0a3a409dd1c18cdcc
Giga <thegogashvili@gmail.com>
2022-04-13 19:08:47 +0400

    initial commit

---
+++ components/input/comments.js
@@ -0,0 +1,81 @@
+import { useState, useEffect, useContext } from "react";
+
+import CommentList from "./comment-list";
+import NewComment from "./new-comment";
+import NotificationContext from "../../store/notification-context";
+import classes from "./comments.module.css";
+
+function Comments(props) {
+  const { eventId } = props;
+
+  const notificationCtx = useContext(NotificationContext);
+
+  const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
+  const [isFetchingComments, setIsFetchingComments] = useState(false);
+
+  useEffect(() => {
+    if (showComments) {
+      setIsFetchingComments(true);
+      fetch(`/api/comments/${eventId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setComments(data.comments);
+          setIsFetchingComments(false);
+        });
+    }
+  }, [showComments]);
+
+  function toggleCommentsHandler() {
+    setShowComments((prevStatus) => !prevStatus);
+  }
+
+  async function addCommentHandler(commentData) {
+    notificationCtx.showNotification({
+      title: "Signing comment...",
+      message: "Your comment is currently being stored into database.",
+      status: "pending",
+    });
+    // send data to API
+    try {
+      const res = await fetch(`/api/comments/${eventId}`, {
+        method: "POST",
+        body: JSON.stringify(commentData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.message || "Something Went Wrong");
+      }
+
+      notificationCtx.showNotification({
+        title: "Success...",
+        message: "Your comment was saved!",
+        status: "success",
+      });
+    } catch (err) {
+      notificationCtx.showNotification({
+        title: "Error...",
+        message: err.message || "Something Weng Wrong!",
+        status: "error",
+      });
+    }
+  }
+
+  return (
+    <section className={classes.comments}>
+      <button onClick={toggleCommentsHandler}>
+        {showComments ? "Hide" : "Show"} Comments
+      </button>
+      {showComments && <NewComment onAddComment={addCommentHandler} />}
+      {showComments && !isFetchingComments && <CommentList items={comments} />}
+      {showComments && isFetchingComments && <p>Loading...</p>}
+    </section>
+  );
+}
+
+export default Comments;
